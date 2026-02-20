36e441e75e1e87431e2ac9c504217a01eb892a82
isabelge <isabel.geissmann@bbz-sh.ch>
2026-02-20 21:40:59 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Struct soll einen Namen und eine Note enthalten, wähle geeignete Datentypen
+struct Studierende {
+    string name;
+    double note;
+};
+
+int main() {
+
+    vector<Studierende> students;
+
+    // Füge 4 Studierende in die Liste students ein
+
+    cout << "Pruefungsergebnisse:" << endl;
+    // Gib alle Studierenden und die jeweilige Note aus
+
+    // Gib aus, ob die jeweilige Person bestanden hat oder nicht
+
+    return 0;
+}
